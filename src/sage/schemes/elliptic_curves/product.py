74f7b8fe6a33246072e8b2ef105dcd5aa45fb966
sferl <sferl@sferl-ThinkPad-X1-Carbon-Gen-12.(none)>
2025-10-14 20:32:34 +0200

    write examples and tests

--- src/sage/schemes/elliptic_curves/product.py
+++ src/sage/schemes/elliptic_curves/product.py
@@ -3,7 +3,7 @@ Products of elliptic curves over a general ring
 
 Given `n` elliptic curves `E_1, \dots, E_n` over `R`,
 their product `E_1 \times \dots \times E_n` is a principally polarized
-abelian variety of dimension `n`. #TODO better docstring?
+abelian variety of dimension `n`. TODO better docstring?
 
 This module defines a parent class :class:`EllipticProduct` to provide support
 for such products, and an element class :class:`EllipticProductPoint` to
@@ -26,9 +26,10 @@ EXAMPLES::
     Implement isogenies between products of elliptic curves:: 
 
         sage: P0, Q0 = E0.torsion_basis(4)
-        sage: prod = EllipticProduct(E0, E0) FIXME is this ok or should I use an actual working example?
+        sage: prod = EllipticProduct(E0, E0)
         sage: prod.isogeny([prod(P0, P0), prod(Q0, Q0)])  # not implemented
 
+#FIXME is this ok or should I use an actual working example?
 AUTHORS:
 
 - Alessandro Sferlazza, Lorenz Panny (2025): Initial version
@@ -38,6 +39,7 @@ AUTHORS:
 # - nicer error messages?
 # - handle products as part of the CartesianProduct category?
 # - write examples and tests in all docstrings
+# - should we really have elliptic curve products under elliptic curves or in a separate folder/module?
 
 import sage.all
 from sage.structure.element import AdditiveGroupElement
@@ -57,7 +59,7 @@ def _unpack(packed): # packed is a tuple
     as arguments, either packed together in a single list/tuple
     or separately as multiple arguments.
     """
-    if len(packed) == 1:
+    if len(packed) == 1 and isinstance(packed[0], (tuple, list)):
         unpacked, = packed
         return tuple(unpacked)
     else:
@@ -74,6 +76,55 @@ class EllipticProduct(Parent, UniqueRepresentation):
         Construct a product of elliptic curves from its factors.
 
         Method used for compatibility with UniqueRepresentation.
+
+        INPUT:
+
+        - `curves`: either multiple arguments, or a single list,
+        where each element is an elliptic curve.
+        
+        EXAMPLES::
+
+            sage: E0 = EllipticCurve(ZZ, [1,0])
+            sage: E1 = EllipticCurve(ZZ, [2,3])
+            sage: A = EllipticProduct(E0, E1, E1, E0)
+
+        The arguments can also be passed as a single tuple or list::
+            
+            sage: AA = EllipticProduct([E0, E1, E1, E0])
+        
+        Initializing an elliptic curve product with the same curves
+        results in the *same* Python object::
+
+            sage: A is AA
+            True
+
+        The base ring of all the curves should be the same::
+
+            sage: EllipticProduct(E0, EllipticCurve(GF(3), [1,0]))
+            Traceback (most recent call last):
+            ...
+            TypeError: all of the given components should be defined over the same base ring
+
+        TESTS::
+
+            sage: Fp2 = GF(419**2, name='i', modulus=var('x')**2 + 1)
+            sage: E0 = EllipticCurve(Fp2, [1,0])
+            sage: E1 = E0.isogenies_prime_degree(2)[0].codomain()
+            sage: A = EllipticProduct(E0, E1)
+            sage: A == EllipticProduct([EllipticCurve(j=E0.j_invariant()),
+            ....:                       E1.identity_morphism().codomain()])
+            True
+
+            sage: EllipticProduct()
+            Traceback (most recent call last):
+            ...
+            ValueError: there must be at least 1 factor
+
+            sage: P = E0.random_point()
+            sage: EllipticProduct(E0, P)
+            Traceback (most recent call last):
+            ...
+            TypeError: all of the given components should be elliptic curves
         """
         return super().__classcall__(cls, *_unpack(curves))
 
@@ -84,8 +135,13 @@ class EllipticProduct(Parent, UniqueRepresentation):
         INPUT:
 
         - ``curves``: a tuple of elliptic curves
+        
+        EXAMPLES::
 
-        TODO in the tests: say there should be at least one component
+            sage: E0 = EllipticCurve(ZZ, [1,0])
+            sage: E1 = EllipticCurve(ZZ, [2,3])
+            sage: A = EllipticProduct(E0, E1); A
+            Product of elliptic curves: (Elliptic Curve defined by y^2 = x^3 + x over Integer Ring, Elliptic Curve defined by y^2 = x^3 + 2*x + 3 over Integer Ring)
         """
         super().__init__(self)
 
@@ -105,12 +161,30 @@ class EllipticProduct(Parent, UniqueRepresentation):
         r"""
         Call the :class:`EllipticProductPoint` constructor
         to initialize a point on this product of elliptic curves.
+
+        See :meth:`EllipticProductPoint.__init__` for more details.
+
+        EXAMPLES::
+
+            sage: F = GF(62207)
+            sage: E0 = EllipticCurve(j=F(1728))
+            sage: A = EllipticProduct(E0, E0)
+            sage: P = E0.random_point()
+            sage: Q = 2 * P
+            sage: PP = A(P, Q, E0(0))
         """
         return EllipticProductPoint(self, *args, **kwds)
     
     def factors(self):
         r"""
         Return the factors of this product of elliptic curves as a tuple.
+
+        EXAMPLES::
+
+            sage: F = GF(62207)
+            sage: E0 = EllipticCurve(j=F(1728)); E1 = EllipticCurve(j=F(0))
+            sage: A = EllipticProduct([E0, E1]); A.factors()
+            (Elliptic Curve defined by y^2 = x^3 + x over Finite Field of size 62207, Elliptic Curve defined by y^2 = x^3 + 1 over Finite Field of size 62207)
         """
         return self._factors  # that's fine because self._curves is a tuple
     
@@ -121,18 +195,56 @@ class EllipticProduct(Parent, UniqueRepresentation):
         INPUT:
 
         - ``n`` -- integer
+
+        EXAMPLES::
+
+            sage: p = random_prime(2, 1000); F = QuadraticField(-p)
+            sage: E0 = EllipticCurve(j=F(0))
+            sage: E1 = EllipticCurve(j=F(1))
+            sage: A = EllipticProduct(E0, E0, E0, E1, E0)
+            sage: A[3].j_invariant()
+            1
+
+        TESTS::
+            sage: A[5]
+            Traceback (most recent call last):
+            ...
+            IndexError: tuple index out of range
         """
         return self._factors[n]
 
     def __len__(self): # TODO needed?
         r"""
         Return the number of factors of this product.
+
+        EXAMPLES::
+
+            sage: p = random_prime(2, 1000); F = GF(p)
+            sage: A = EllipticProduct(
+            ....:         EllipticCurve(j=F(0)),
+            ....:         EllipticCurve(j=F(1)),
+            ....:         EllipticCurve(j=F.random_element())
+            ....:     )
+            sage: len(A)
+            3
         """
         return len(self._factors)
 
     def _repr_(self):
         r"""
         Return a string representation of this product of elliptic curves.
+        
+        EXAMPLES::
+
+            sage: p = 419
+            sage: E = EllipticCurve(GF(p), [1,0])
+            sage: A = EllipticProduct(E, E); A
+            Product of elliptic curves: (Elliptic Curve defined by y^2 = x^3 + x over Finite Field of size 419, Elliptic Curve defined by y^2 = x^3 + x over Finite Field of size 419)
+        
+        TESTS::
+
+            sage: EE = EllipticProduct(E); EE
+            Product of elliptic curves: (Elliptic Curve defined by y^2 = x^3 + x over Finite Field of size 419,)
         """
         return "Product of elliptic curves: " + str(self._factors)
 
@@ -141,6 +253,20 @@ class EllipticProduct(Parent, UniqueRepresentation):
         Compare two elliptic curve products.
         
         This is done by comparing the underlying tuples of factors.
+
+        EXAMPLES::
+
+            sage: Fp2 = GF(419**2, name='i', modulus=var('x')**2 + 1)
+            sage: E0 = EllipticCurve(Fp2, [1,0])
+            sage: E1 = E0.isogenies_prime_degree(2)[0].codomain()
+            sage: A = EllipticProduct(E0, E1)
+            sage: A == EllipticProduct([EllipticCurve(j=E0.j_invariant()),
+            ....:                       E1.identity_morphism().codomain()])
+            True
+            sage: A == (E0, E1)  # an elliptic curve product is not a tuple
+            False
+            sage: A != EllipticProduct(E1, E1)
+            True
         """
         if not isinstance(other, EllipticProduct):
             return NotImplemented
@@ -152,6 +278,16 @@ class EllipticProduct(Parent, UniqueRepresentation):
 
         Since every factor of the product has dimension 1,
         the output matches the output of :meth:`__len__`.
+
+        EXAMPLES::
+
+            sage: p = random_prime(2, 1000); F = GF(p)
+            sage: E0 = EllipticCurve(j=F(0))
+            sage: E1 = EllipticCurve(j=F(1))
+            sage: A = EllipticProduct(E0); A.dimension()
+            1
+            sage: A = EllipticProduct([E0, E0, E0, E1, E0]); A.dimension()
+            5
         """
         return len(self._factors)
     
@@ -160,6 +296,16 @@ class EllipticProduct(Parent, UniqueRepresentation):
         Return the base ring of ``self``.
 
         This is the common base ring of all factors of the product.
+
+        EXAMPLES::
+
+            sage: p = 167; F = Qp(p, prec=20)
+            sage: E = EllipticCurve(j=F(0))
+            sage: A = EllipticProduct(E, E, E); A.base_ring()
+            167-adic Field with capped relative precision 20
+            sage: EE = E.change_ring(F.residue_field())
+            sage: EllipticProduct(EE, EE).base_ring()
+            Finite Field of size 167
         """
         return self._base_ring
     
@@ -168,7 +314,23 @@ class EllipticProduct(Parent, UniqueRepresentation):
         Return the base field of ``self``.
 
         This is the common base field where all the factors are defined.
+
+        EXAMPLES::
+
+            sage: p = 167; F = GF(p)
+            sage: E = EllipticCurve(j=F(0))
+            sage: A = EllipticProduct(E, E, E); A.base_field()
+            Finite Field of size 167
+            sage: E1 = E.change_ring(Zmod(p))
+            sage: EllipticProduct(E1, E1, E1).base_field()
+            Ring of integers modulo 167
+            sage: E2 = EllipticCurve(ZZ, [1,0])
+            sage: EllipticProduct(E2, E2).base_field()
+            Traceback (most recent call last):
+            ...
+            ValueError: elliptic curve product not defined over a field
         """
+        # FIXME need it? should it be an alias of base_ring? should I check instead that the children are instances of EllipticCurve_field?
         if self._base_ring.is_field():
             return self._base_ring
         else:
@@ -177,6 +339,16 @@ class EllipticProduct(Parent, UniqueRepresentation):
     def random_element(self):
         r"""
         Return a random point on this elliptic curve product.
+
+        EXAMPLES::
+
+            sage: p = random_prime(1000)
+            sage: F = GF(p**2, name="a")
+            sage: curves = [EllipticCurve(j=F.random_element())
+            ....:   for _ in range(2)]
+            sage: PP = EllipticProduct(curves).random_element()
+            sage: PP in EllipticProduct(curves)  # random
+            Point ((333*a + 36 : 225*a + 629 : 1), (590*a + 387 : 712*a + 703 : 1)) on Product of elliptic curves: (Elliptic Curve defined by y^2 = x^3 + (710*a+529)*x + (661*a+7) over Finite Field in a of size 787^2, Elliptic Curve defined by y^2 = x^3 + (290*a+525)*x + (438*a+354) over Finite Field in a of size 787^2)
         """
         return self(*(curve.random_element() for curve in self._factors))
     
@@ -185,6 +357,14 @@ class EllipticProduct(Parent, UniqueRepresentation):
     def j_invariants(self):
         r"""
         Return the j-invariant of each factor of the product as a tuple.
+
+        EXAMPLES::
+
+            sage: p = 419; F = GF(p**2, name="a")
+            sage: E0 = EllipticCurve(F, j=0)
+            sage: E1 = EllipticCurve(F, [1,0])
+            sage: EllipticProduct(E0, E1).j_invariants() == (0, 1728)
+            True  
         """
         return tuple(curve.j_invariant() for curve in self._factors)
     
@@ -209,7 +389,43 @@ class EllipticProductPoint(AdditiveGroupElement):
 
         - ``components``: either a list, or multiple arguments,
         where each element can be converted into a point on the corresponding
-        elliptic curve in the parent product. FIXME strange phrasing
+        elliptic curve. The number of components should match the number of 
+        factors of the parent product. FIXME strange phrasing
+
+        EXAMPLES::
+
+            sage: F = GF(62207)
+            sage: E0 = EllipticCurve(j=F(1728))
+            sage: A = EllipticProduct(E0, E0, E0)
+            sage: P = E0.random_point()
+            sage: Q = 2 * P
+            sage: PP = A(P, Q, E0(0))
+            sage: all(P in E0 for P in PP)
+            True
+
+        The arguments can also be passed as a single tuple or list::
+
+            sage: PP == A([P, Q, E0(0)])
+            True
+
+        The arguments can be anything that is accepted by the elliptic curve
+        point constructor on the corresponding curve::
+
+            sage: PP == A([P.x(), P.y()], Q, 0)
+            True
+
+        TESTS::
+
+            sage: A(P)
+            Traceback (most recent call last):
+            ...
+            TypeError: number of points does not match parent dimension
+
+            sage: B = EllipticCurve(E0, E0)
+            sage: B([P.x(), P.y()])
+            Traceback (most recent call last):
+            ...
+            TypeError: v (=(51727,)) must have 3 components
         """
         super().__init__(parent)
 
@@ -414,8 +630,8 @@ if __name__ == "__main__":
     P0, P1 = PP
     assert PP == prod2(P0, P1)
     assert PP == prod2([P0, P1])
-    assert prod2.lift_x(P0.x(), P1.x()) in (PP, -PP, prod2(P0, -P1), prod2(-P0, P1))
-    assert prod2.lift_x([P0.x(), P1.x()]) == prod2.lift_x(P0.x(), P1.x())
+    # assert prod2.lift_x(P0.x(), P1.x()) in (PP, -PP, prod2(P0, -P1), prod2(-P0, P1))
+    # assert prod2.lift_x([P0.x(), P1.x()]) == prod2.lift_x(P0.x(), P1.x())
 
     try:
         prod2(P0)
